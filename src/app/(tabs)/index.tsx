f3094cd62a0ce572fd28dbcbc49f6475f30e8b61
Bibhash Dash <bibhashdash@gmail.com>
2026-05-04 11:36:01 +0100

    copy and share

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import { globalStyles } from '@/styles/global';
-import { Text, ScrollView } from 'react-native';
+import {Text, ScrollView, View} from 'react-native';
 import HomeHeader from '../../components/HomeHeader';
 import MacroGrid from "@/components/MacroGrid";
 import RecentMeals from "@/components/RecentMeals";
 import {getMeals, Meal} from "@/storage/meals";
 import {useCallback, useState} from "react";
 import {useFocusEffect} from "expo-router";
+import ShareButton from "@/components/ShareButton";
+import CopyButton from "@/components/CopyButton";
+import ReminderToggle from "@/components/ReminderToggle";
 
 export default function HomeScreen() {
     const [meals, setMeals] = useState<Array<Meal>>([])
@@ -22,9 +25,14 @@ export default function HomeScreen() {
     );
     return (
         <ScrollView style={globalStyles.container}>
-            <Text style={globalStyles.title}>MacroZone</Text>
+            <View style={globalStyles.header}>
+                <Text style={globalStyles.title}>MacroZone</Text>
+                <ShareButton meals={meals} />
+            </View>
             <HomeHeader />
             <MacroGrid meals={meals} />
+            <CopyButton meals={meals} />
+            {/*<ReminderToggle />*/}
             <RecentMeals meals={meals} onDelete={loadMeals}/>
         </ScrollView>
     );
