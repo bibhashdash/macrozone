7862f35b3c6c0d6d121b55d0a60eb1dea881b22c
Bibhash Dash <bibhashdash@gmail.com>
2026-05-04 11:03:11 +0100

    initial commit lol

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { globalStyles } from '@/styles/global';
+import { Text, ScrollView } from 'react-native';
+import HomeHeader from '../../components/HomeHeader';
+import MacroGrid from "@/components/MacroGrid";
+import RecentMeals from "@/components/RecentMeals";
+import {getMeals, Meal} from "@/storage/meals";
+import {useCallback, useState} from "react";
+import {useFocusEffect} from "expo-router";
+
+export default function HomeScreen() {
+    const [meals, setMeals] = useState<Array<Meal>>([])
+    const loadMeals = async () => {
+        const data = await getMeals();
+        setMeals(data);
+        console.log('Loaded meals:', data);
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            loadMeals();
+        }, []),
+    );
+    return (
+        <ScrollView style={globalStyles.container}>
+            <Text style={globalStyles.title}>MacroZone</Text>
+            <HomeHeader />
+            <MacroGrid />
+            <RecentMeals meals={meals}/>
+        </ScrollView>
+    );
+}
